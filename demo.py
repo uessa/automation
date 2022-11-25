73d0ae5a650eb0fc002e40c4e19212e56c3f611a
minato uesaka <minato@muesaka.local>
2022-11-25 18:58:25 +0900

    demo.py 修正

--- demo.py
+++ demo.py
@@ -1,27 +1,31 @@
 import deepl
 import glob
+import os
 
 '''
 ・以下のサイトを参照
-    https://qiita.com/Chanmoro/items/9a3c86bb465c1cce738a   # "10分で理解するSelenium"
-    https://qiita.com/fujino-fpu/items/e94d4ff9e7a5784b2987 # "PythonとSeleniumでDeepLに英文流して自動翻訳させる"
-    https://www.hamlet-engineer.com/posts/DeepL.html        # "Python + Selenium + ChromeでDeepLの翻訳を実行する"
+    https://qiita.com/Chanmoro/items/9a3c86bb465c1cce738a           # "10分で理解するSelenium"
+    https://qiita.com/fujino-fpu/items/e94d4ff9e7a5784b2987         # "PythonとSeleniumでDeepLに英文流して自動翻訳させる"
+    https://www.hamlet-engineer.com/posts/DeepL.html                # "Python + Selenium + ChromeでDeepLの翻訳を実行する"
     https://yuki.world/python-selenium-chromedriver-auto-update/    # "ChromeDriverを自動更新するPythonライブラリが便利"
-・以下のdockerイメージで動作
+・dockerを使う場合は以下で動作
     docker run -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:3.141.59-xenon
-・コンテナ内で？ブラウザ動作してるからローカルでブラウザ動作の確認ができない
-・deepl.py実行時にwarningが出る
-    -> demo.pyでdeeplをモジュール読み込みすると出ない？
-・while True文 + try exept文でsleep(1)で回してるっぽいので, BOT扱いされちゃうかも？
-・text = deepl.TranslationByDeepl(text)でtextが翻訳されて返ってくる
+・deepl.pyのみを実行時にwarningが出る
+    -> demo.pyでdeeplをモジュール読み込みすると出ない
 
 '''
 
 if __name__ == "__main__":
 
-    pre_filepath = glob.glob("*.txt")[0] # ディレクトリの.txtファイルを参照
-    post_filepath = "translated/(translated)" + pre_filepath # translatedディレクトリに保存
+    pre_filepath = glob.glob("*.txt")[0] # 翻訳対象
+    post_dir = "./Translated" # 保存先のディレクトリ
+    os.makedirs(post_dir, exist_ok=True) # ディレクトリがなければ作る
+    post_filepath = "{}/(Translated){}".format(post_dir, pre_filepath) # 保存先のパス
 
+    # 確認
+    print("pre_file:   \"{}\"".format(pre_filepath))
+
+    # テキストを分割してリストに
     pre_text, sep_text = deepl.divide_readable_text(pre_filepath) # ファイル（Readable）を英文と区切り文字のリストに分割
     post_text = []
 
@@ -30,10 +34,13 @@ if __name__ == "__main__":
         # if i == 0:
         #     break
         
+        # 翻訳
         post_text.append(deepl.translate_by_deepl(pre_text[i])) # 翻訳した英文を保存
         post_text.append(sep_text[i]) # 区切り文字を保存
         print("{}/{} finished".format(i+1, len(pre_text)))
         print("")
+    
+
 
     # 翻訳したテキスト（Readable）をファイルに書き込み
     with open(post_filepath, 'w') as f:
@@ -41,6 +48,5 @@ if __name__ == "__main__":
             f.write("{}\n".format(text)) # 改行して書き込み
 
     # 確認
-    print("pre_file = {}".format(pre_filepath))
-    print("post_file = {}".format(post_filepath))
+    print("post_file:  \"{}\"".format(post_filepath))
 
